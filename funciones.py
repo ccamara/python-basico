7907ac2b283f8283be41acddf05b461ac76a28bd
Carlos Cámara <carlos.camara@gmail.com>
2014-02-08 00:31:47 +0100

    Updated functiones.py

--- funciones.py
+++ funciones.py
@@ -1,6 +1,7 @@
 # Listado de funciones comunes
 print() #Imprime en pantalla el valor entre paréntesis
-int()  #Fuerza al intérprete a user el valor entre paréntesis como número, no como texto
+int() # Fuerza a que el resultado sea un entero
+str() # Convierte un número en una cadena de texto
 input("Texto aquí:") #Permite introducir valores con el teclado
 
 help(print) # Muestra el manual en línea sobre la función entre paréntesis
