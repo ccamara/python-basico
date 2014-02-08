3b930a02fd055b9c2c6dbf2736ae8707efc521d7
Carlos Cámara <carlos.camara@gmail.com>
2014-02-08 11:37:47 +0100

    Added funciones folder

---
+++ 03 funciones/01-funciones.py
@@ -0,0 +1,8 @@
+name = input("Escribe tu nombre: ")
+
+def saluda(name):
+    print("muy buenos dias " + name ) # End of function
+    # bloque de la función
+    # podríamos añadir lo que quisiéramos
+
+saluda(name) # Ejecutamos la función
