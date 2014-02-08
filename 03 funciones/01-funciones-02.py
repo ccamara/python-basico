ac9640774a919a12b40118f50847b7247efb484f
Carlos Cámara <carlos.camara@gmail.com>
2014-02-08 13:55:07 +0100

    Added funciones file

---
+++ 03 funciones/01-funciones-02.py
@@ -0,0 +1,14 @@
+def area_rectang(base, height):
+    area = base * height
+    print("El area de un rectangulo de base= %d y altura= %d es: %d" %(base, height,area))
+
+# Llamamos a la función para que calcule
+
+# Parámetros como números
+area_rectang(2,3)
+
+# Parámetros como variables
+# Las variables pueden ser las mismas que en la función u otras
+base = 5  
+altura = 6 
+area_rectang(base,altura)
