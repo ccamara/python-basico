510737b957694ead5a74941168b126bdfb40db4d
Carlos Cámara <carlos.camara@gmail.com>
2014-02-08 23:50:53 +0100

    Added 02-funciones-variables.py

--- 03 funciones/02-funciones-variables.py
+++ 03 funciones/02-funciones-variables.py
@@ -2,9 +2,9 @@ x = 50
 
 def func(x):
   # Vamos a cambiar el valor de x dentro de esta función
-  print("x es", x)
+  print("x es", x) # Muestra el valor de la variable global
   x = 2 # Nueva variable local que solo existe dentro de esta función y se llama x
   print("Cambiamos la x local a", x)
 
 func(x) 
-print("x sigue siendo", x) # La variable x no se ha alterado
+print("x sigue siendo", x) # La variable global x no se ha alterado
