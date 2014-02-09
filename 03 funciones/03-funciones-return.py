c71c54b3524b19d6688d7064960d94b6ac2f2428
Carlos Cámara <carlos.camara@gmail.com>
2014-02-09 20:06:23 +0100

    Update 03-funciones-return.py

--- 03 funciones/03-funciones-return.py
+++ 03 funciones/03-funciones-return.py
@@ -7,5 +7,6 @@ def maximum(x, y):
     return y
   print("Esto no debería ejecutarse nunca")
   # No se ejecuta porque las tres opciones anteriores fuerzan la salida de la función
+  # Solo se ejecutaría si alguna de las opciones anteriores tuviese un print en lugar de return
   
 print(maximum(2, 3))
