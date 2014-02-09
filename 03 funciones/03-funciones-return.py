460c2510d3a3e22af099981cdf64f3ec6b68b04a
Carlos Cámara <carlos.camara@gmail.com>
2014-02-09 20:05:48 +0100

    Update 03-funciones-return.py

--- 03 funciones/03-funciones-return.py
+++ 03 funciones/03-funciones-return.py
@@ -5,5 +5,7 @@ def maximum(x, y):
     return "Los números son iguales"
   else:
     return y
+  print("Esto no debería ejecutarse nunca")
+  # No se ejecuta porque las tres opciones anteriores fuerzan la salida de la función
   
 print(maximum(2, 3))
