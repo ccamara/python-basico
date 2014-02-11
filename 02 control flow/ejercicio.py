0dde1847d50147089feedb6cd42f78c0a21580c0
Carlos Cámara <carlos.camara@gmail.com>
2014-02-11 17:39:29 +0100

    Refactoring ejercicio.py

--- 02 control flow/ejercicio.py
+++ 02 control flow/ejercicio.py
@@ -6,25 +6,26 @@ guess = input("Escribe un número del 1 al 10: ")
 count = 1
 
 # Verificamos que guess no contenga carácteres no numéricos
-if guess is not guess.isdigit():
-  print("Solo puedes escribir números enteros")
-  count = count +1
-  guess = input("Escribe un número del 1 al 10: ")
-
-guess = int(guess) # Convertimos guess en un número
-
-while guess != secret:
-  if guess > 10:
-    print("El número elegido no está entre 1 y 10")
-    count = count + 1
-    guess = int(input("Escribe un número del 1 al 10: "))
-  else:
-    if guess > secret:
-      print("Demasiado alto, inténtalo de nuevo")
+if guess.isdigit():
+  guess = int(guess) # Convertimos guess en un número
+  while guess != secret:
+    if guess > 10:
+      print("El número elegido no está entre 1 y 10")
       count = count + 1
       guess = int(input("Escribe un número del 1 al 10: "))
     else:
-      print("Demasiado bajo")
-      count = count + 1
-      guess = int(input("Escribe un número del 1 al 10: "))
-print("¡Enhorabuena!, lo has conseguido en " + str(count) + " intentos")
+      if guess > secret:
+        print("Demasiado alto, inténtalo de nuevo")
+        count = count + 1
+        guess = int(input("Escribe un número del 1 al 10: "))
+      else:
+        print("Demasiado bajo")
+        count = count + 1
+        guess = int(input("Escribe un número del 1 al 10: "))
+  print("¡Enhorabuena!, lo has conseguido en " + str(count) + " intentos")
+
+else:
+  # Si la condición no se cumple es que hay algo que no son números
+  print("Solo puedes escribir números enteros")
+  count = count +1
+  guess = input("Escribe un número del 1 al 10: ")
