63d3ebb4a61d4373801c48b2f1d25c37a2957443
Carlos Cámara <carlos.camara@gmail.com>
2014-02-11 17:37:06 +0100

    Added documentation to function

--- 03 funciones/01-funciones.py
+++ 03 funciones/01-funciones.py
@@ -1,6 +1,8 @@
 def area_rectang(base, height):
-    area = base * height
-    print("El area de un rectangulo de base= %d y altura= %d es: %d" %(base, height,area))
+  '''Calcula el área de un cuadrado, a partir de dos parámetros: base y altura.
+  '''
+  area = base * height
+  print("El area de un rectangulo de base= %d y altura= %d es: %d" %(base, height,area))
 
 # Llamamos a la función para que calcule
 
@@ -12,3 +14,5 @@ area_rectang(2,3)
 base = 5  
 altura = 6 
 area_rectang(base,altura)
+
+area_rectang.__doc__
