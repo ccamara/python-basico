a21ec0e4006ba04c2067344bbc519acd0df380ba
Carlos Cámara <carlos.camara@gmail.com>
2014-02-20 19:02:53 +0100

    Added more for examples

--- 02 control flow/04-for.py
+++ 02 control flow/04-for.py
@@ -2,3 +2,13 @@ for i in range(1, 5):
     print(i)
 else:
     print("The for loop is over")
+
+# Ejemplo con listas, sacado de http://www.codecademy.com/courses/python-beginner-en-pwmb1/1/6
+start_list = [5, 3, 1, 2, 4]
+square_list = []
+
+for number in start_list:
+    number = number ** 2
+    square_list.append(number)
+square_list.sort()
+print(square_list)
