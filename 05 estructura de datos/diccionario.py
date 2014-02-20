71b9b65dd82d443ef0297a69ba51850c6441ba9f
Carlos Cámara <carlos.camara@gmail.com>
2014-02-20 20:57:11 +0100

    Added more dictionary examples

--- 05 estructura de datos/diccionario.py
+++ 05 estructura de datos/diccionario.py
@@ -1,3 +1,21 @@
-dictionary = "adsfasdf"
+# Sintaxis: nombre_diccionario = { llave1: valor1, llave2: valor2}
+# Llave = nombre : valor = usuario de twitter
+twitter_users = {"carlos":"@carlescamara", "ETSA USJ":"@etsa_usj"}
+precios_software = {} #Creamos un diccionario vacío, ya lo llenaremos más tarde
 
-format("Carlos")
+# Añadir entradas
+# Llave = nombre software : valor = precio
+twitter_users["USJ"]= "@_usj_"
+precios_software["Autocad"] = 3000
+precios_software["Blender"] = 0
+precios_software["SketchupPro"] = 500
+precios_software["Witbox 3D"] = 2000
+
+print(precios_software)
+
+# Eliminar registros
+print("\nEliminamos la entrada Witbox 3D")
+
+del precios_software["Witbox 3D"]
+
+print(precios_software)
